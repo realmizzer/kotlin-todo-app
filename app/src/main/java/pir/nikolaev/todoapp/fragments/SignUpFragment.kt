a5ab8dd754cdb668c073049f0393ebdc719e7cbc
realMiZZeR <maxcan2work@gmail.com>
2024-10-01 23:39:22 +0700

    added logic to sign_up fragment

--- app/src/main/java/pir/nikolaev/todoapp/fragments/SignUpFragment.kt
+++ app/src/main/java/pir/nikolaev/todoapp/fragments/SignUpFragment.kt
@@ -5,16 +5,70 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import com.google.firebase.auth.FirebaseAuth
 import pir.nikolaev.todoapp.R
+import pir.nikolaev.todoapp.databinding.FragmentSignUpBinding
 
 class SignUpFragment : Fragment() {
+
+    private lateinit var auth: FirebaseAuth
+    private lateinit var navController: NavController
+    private lateinit var binding: FragmentSignUpBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sign_up, container, false)
+
+        binding = FragmentSignUpBinding.inflate(inflater, container, false)
+        return binding.root
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
+        super.onViewCreated(view, savedInstanceState)
+
+        init(view)
+        registerEvents()
+
+    }
+
+    private fun init(view: View) {
+
+        navController = Navigation.findNavController(view)
+        auth = FirebaseAuth.getInstance()
+
+    }
+
+    private fun registerEvents() {
+
+        binding.tvHaveAccount.setOnClickListener {
+            navController.navigate(R.id.action_signUpFragment_to_signInFragment)
+        }
+
+        binding.buttonSignUp.setOnClickListener {
+            val email = binding.etEmail.text.toString().trim()
+            val password = binding.etPassword.text.toString().trim()
+            val verifyPassword = binding.etVerifyPassword.text.toString().trim()
+
+            if (email.isEmpty() || password.isEmpty() || verifyPassword.isEmpty()) return@setOnClickListener
+            if (password != verifyPassword) return@setOnClickListener
+
+            auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener {
+                if (!it.isSuccessful) {
+                    Toast.makeText(context, it.exception?.message, Toast.LENGTH_SHORT).show()
+                    return@addOnCompleteListener
+                }
+
+                navController.navigate(R.id.action_signUpFragment_to_homeFragment)
+                Toast.makeText(context, "Sign Up was completed successfully", Toast.LENGTH_SHORT).show()
+            }
+        }
+
     }
 
 }
