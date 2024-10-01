9248a36ea36c0ee23455292b5fc91674ccca93ac
realMiZZeR <maxcan2work@gmail.com>
2024-10-01 23:39:11 +0700

    added logic to sign_in fragment

--- app/src/main/java/pir/nikolaev/todoapp/fragments/SignInFragment.kt
+++ app/src/main/java/pir/nikolaev/todoapp/fragments/SignInFragment.kt
@@ -5,56 +5,68 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import com.google.firebase.auth.FirebaseAuth
 import pir.nikolaev.todoapp.R
+import pir.nikolaev.todoapp.databinding.FragmentSignInBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [SignInFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class SignInFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var auth: FirebaseAuth
+    private lateinit var navController: NavController
+    private lateinit var binding: FragmentSignInBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sign_in, container, false)
+
+        binding = FragmentSignInBinding.inflate(inflater, container, false)
+        return binding.root
+
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment SignInFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            SignInFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
+        super.onViewCreated(view, savedInstanceState)
+
+        init(view)
+        registerEvents()
+
     }
+
+    private fun init(view: View) {
+
+        navController = Navigation.findNavController(view)
+        auth = FirebaseAuth.getInstance()
+
+    }
+
+    private fun registerEvents() {
+
+        binding.tvHaveNoAccount.setOnClickListener {
+            navController.navigate(R.id.action_signInFragment_to_signUpFragment)
+        }
+
+        binding.buttonSignIn.setOnClickListener {
+            val email = binding.etEmail.text.toString().trim()
+            val password = binding.etPassword.text.toString().trim()
+
+            if (email.isEmpty() || password.isEmpty()) return@setOnClickListener
+
+            auth.signInWithEmailAndPassword(email, password).addOnCompleteListener {
+                if (!it.isSuccessful) {
+                    Toast.makeText(context, it.exception?.message, Toast.LENGTH_SHORT).show()
+                    return@addOnCompleteListener
+                }
+
+                navController.navigate(R.id.action_signInFragment_to_homeFragment)
+                Toast.makeText(context, "Sign In was completed successfully", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+    }
+
 }
