230fba92a1263847031605a844972463e28d5b06
realmizzer <maxcan2work@gmail.com>
2024-11-27 01:55:18 +0700

    implemented dialog fragment for a todo create

--- app/src/main/java/pir/nikolaev/todoapp/fragments/AddTodoDialogFragment.kt
+++ app/src/main/java/pir/nikolaev/todoapp/fragments/AddTodoDialogFragment.kt
@@ -5,22 +5,55 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.DialogFragment
+import com.google.android.material.textfield.TextInputEditText
+import pir.nikolaev.todoapp.R
 import pir.nikolaev.todoapp.databinding.FragmentAddTodoDialogBinding
 
-class AddTodoDialogFragment : Fragment() {
+class AddTodoDialogFragment : DialogFragment() {
 
     private lateinit var binding: FragmentAddTodoDialogBinding
+    private lateinit var listener: IAddTodoDialogListener
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-
         binding = FragmentAddTodoDialogBinding.inflate(inflater, container, false)
         return binding.root
-
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
+        registerEvents()
+    }
+
+    private fun registerEvents() {
+        binding.btnClose.setOnClickListener {
+            dismiss()
+        }
+
+        binding.btnAddTodo.setOnClickListener {
+            val todoText = binding.etTodoTitle.text.toString().trim();
+
+            if (todoText.isEmpty()) {
+                Toast.makeText(context, R.string.empty_todo_text, Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            listener.onSaveTask(todoText, binding.etTodoTitle)
+            dismiss()
+        }
+    }
+
+    fun setListener(listener: IAddTodoDialogListener) {
+        this.listener = listener
+    }
+
+    interface IAddTodoDialogListener {
+        fun onSaveTask(text: String, et: TextInputEditText)
+    }
 
 }
--- app/src/main/java/pir/nikolaev/todoapp/fragments/HomeFragment.kt
+++ app/src/main/java/pir/nikolaev/todoapp/fragments/HomeFragment.kt
@@ -5,19 +5,23 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
+import com.google.android.material.textfield.TextInputEditText
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
+import pir.nikolaev.todoapp.R
 import pir.nikolaev.todoapp.databinding.FragmentHomeBinding
 
-class HomeFragment : Fragment() {
+class HomeFragment : Fragment(), AddTodoDialogFragment.IAddTodoDialogListener {
 
     private lateinit var auth: FirebaseAuth
     private lateinit var databaseRef: DatabaseReference
     private lateinit var navController: NavController
     private lateinit var binding: FragmentHomeBinding
+    private lateinit var addTodoDialog: AddTodoDialogFragment
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -33,15 +37,39 @@ class HomeFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         init(view)
-
+        registerEvents()
     }
 
     private fun init(view: View) {
 
         auth = FirebaseAuth.getInstance()
         databaseRef = FirebaseDatabase.getInstance().reference
+            .child("Tasks")
+            .child(auth.currentUser?.uid.toString())
         navController = Navigation.findNavController(view)
 
+    }
+
+    private fun registerEvents() {
+        binding.btnAdd.setOnClickListener {
+            addTodoDialog = AddTodoDialogFragment()
+            addTodoDialog.setListener(this)
+            addTodoDialog.show(
+                childFragmentManager,
+                "AddTodoDialogFragment"
+            )
+        }
+    }
+
+    override fun onSaveTask(text: String, et: TextInputEditText) {
+        databaseRef.push().setValue(text).addOnCompleteListener {
+            if (!it.isSuccessful) {
+                Toast.makeText(context, it.exception?.message, Toast.LENGTH_LONG).show()
+                return@addOnCompleteListener
+            }
+
+            Toast.makeText(context, R.string.sign_in_success, Toast.LENGTH_SHORT).show()
+        }
     }
 
 }
